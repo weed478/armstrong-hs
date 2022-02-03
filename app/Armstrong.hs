f6cd526a9f63c7f259d15a3f0f3e322a9201b2bb
Carbon225 <carbon225@protonmail.com>
2022-02-03 20:52:10 +0100

    oh well

--- app/Armstrong.hs
+++ app/Armstrong.hs
@@ -2,36 +2,32 @@ module Armstrong
     ( armstrongNumbers
     ) where
 
-import Control.Monad ( guard )
-import Control.Applicative ( Alternative )
+import Control.Monad ( guard, MonadPlus )
 import Math.NumberTheory.Primes.Testing ( millerRabinV )
-import Data.WideWord.Int128 ( Int128 )
 
-type Wide = Int128
-
-isPrime :: Integral a => a -> Bool
-isPrime = millerRabinV 5 . toInteger
+isPrime :: Integer -> Bool
+isPrime = millerRabinV 5
 
 digits :: Integral a => a -> [a]
-digits n = let (m, d) = divMod n 10 in d : (guard (m > 0) >> digits m)
+digits n = let (m, d) = n `quotRem` 10 in d : (guard (m > 0) >> digits m)
 
-getDigitmap :: (Integral a, Num b) => a -> b -> b
-getDigitmap n nDigits = sum . map ((nDigits + 1)^) . digits $ n
+getDigitmap :: (Num a, Integral b) => a -> b -> a
+getDigitmap radix = sum . map (radix ^) . digits
 
-getSump :: (Integral a, Integral b) => a -> a -> b -> a
-getSump digitmap nDigits len = loop (digitmap `div` (nDigits + 1)) 1
-    where loop _ 10 = 0
-          loop dm d = let (nextDm, dCount) = dm `divMod` (nDigits + 1) 
-                      in dCount * d ^ len + loop nextDm (d + 1)
+getSump :: (Integral a, Integral b) => a -> b -> a -> a
+getSump radix len digitmap = fst $ foldl loop (0, digitmap `quot` radix) [1..9]
+    where loop (sump, dm) d = let (nextDm, dCount) = dm `quotRem` radix
+                              in (sump + dCount * d ^ len, nextDm)
 
-testSump :: (Integral a, Integral b, Monad m, Alternative m) => a -> a -> b -> m a
-testSump digitmap nDigits len = guard (isPrime sump && getDigitmap sump nDigits == digitmap) >> return sump
-    where sump = getSump digitmap nDigits len
+getArmstrong :: (Integral a, MonadPlus m) => Integer -> a -> Integer -> m Integer
+getArmstrong radix len digitmap = guard isArmstrong >> return sump
+    where sump = getSump radix len digitmap
+          isArmstrong = isPrime sump && getDigitmap radix sump == digitmap
 
-forAllCombos :: (Integral a, Num b, Eq b) => a -> b -> a -> a -> [a]
-forAllCombos nDigits 0 digitmap cLen =  testSump digitmap nDigits cLen
-forAllCombos nDigits depth digitmap cLen = [0..nDigits-cLen] >>= \i ->
-    forAllCombos nDigits (depth - 1) (digitmap * (nDigits + 1) + i) (cLen + i)
+forAllCombos :: Integer -> Integer -> Integer -> Int -> [Integer]
+forAllCombos radix len digitmap 0 = getArmstrong radix len digitmap
+forAllCombos radix len digitmap depth = [0..radix-len-1] >>= \i ->
+    forAllCombos radix (len + i) (digitmap * radix + i) (depth - 1)
 
-armstrongNumbers :: [Wide]
-armstrongNumbers = forAllCombos 23 10 0 0
+armstrongNumbers :: [Integer]
+armstrongNumbers = take 7 $ forAllCombos 24 0 0 10
